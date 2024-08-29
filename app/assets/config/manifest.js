9d4d47664bde82410946fa5eedb87864d996f636
MagalhaesExe <alexmagalhaesjr15@gmail.com>
2024-08-28 23:27:26 -0300

    teste

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,11 +2,9 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-<<<<<<< HEAD
-=======
 //= link login.js
 //= link products.js
 //= link products.css
 //= link profile.js
 //= link admin_users.js
->>>>>>> 88ac1b4 (teste)
+
